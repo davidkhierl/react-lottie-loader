2ae648d30100386c96e028d9dfde8a8cb369fc7d
David Khierl <dkrgidor@gmail.com>
2023-02-08 07:19:09 +0400

    feat: player ref Enables access to lottie player and animation item ref directly

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,62 @@
-import lottie, { AnimationConfig } from "lottie-web";
-import { useEffect, useRef } from "react";
+import lottie, {
+  AnimationConfig,
+  AnimationItem,
+  LottiePlayer as AnimationPlayer,
+} from "lottie-web";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 
 import React from "react";
 
 export interface LottieLoaderProps extends Omit<AnimationConfig, "container"> {
   animationData: any;
   className?: string;
+  style?: React.HTMLAttributes<HTMLDivElement>["style"];
 }
 
-const LottieLoader = ({ className, ...props }: LottieLoaderProps) => {
-  const divRef = useRef<HTMLDivElement>(null);
+export type LottiePlayer =
+  | (AnimationPlayer & { animationItem?: AnimationItem })
+  | undefined;
 
-  useEffect(() => {
-    lottie.loadAnimation({
-      ...props,
-      container: divRef.current as HTMLDivElement,
-    });
-  }, []);
+const LottieLoader = forwardRef<LottiePlayer, LottieLoaderProps>(
+  ({ className, style, ...props }, ref) => {
+    const containerRef = useRef<HTMLDivElement>(null);
 
-  return <div className={className ? className : ""} ref={divRef}></div>;
-};
+    const lottieRef = useRef<LottiePlayer>();
+
+    useEffect(() => {
+      if (!containerRef.current) return;
+
+      const animationItem = lottie.loadAnimation({
+        ...props,
+        container: containerRef.current,
+      });
+
+      lottieRef.current = { ...lottie, animationItem };
+
+      return () => {
+        lottieRef.current?.destroy();
+        lottieRef.current?.animationItem?.destroy();
+      };
+    }, [props.animationData]);
+
+    useImperativeHandle(
+      ref,
+      () => {
+        return lottieRef.current;
+      },
+      [lottieRef.current]
+    );
+
+    return (
+      <div
+        ref={containerRef}
+        className={className ? className : ""}
+        style={style}
+      ></div>
+    );
+  }
+);
+
+LottieLoader.displayName = "LottieLoader";
 
 export default LottieLoader;
